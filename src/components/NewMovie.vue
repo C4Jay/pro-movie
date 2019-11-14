804efb833f97bb0e26c1d90e398ec6d39b752f5b
C4Jay <choolakejay@gmail.com>
2019-11-14 10:19:42 +0530

    init:

---
+++ src/components/NewMovie.vue
@@ -0,0 +1,10 @@
+mounted () {
+  axios
+    .get('http://www.omdbapi.com/?s=mummy&apikey=XXXXX&page=1&type=movie&Content-Type=application/json')
+    .then(response => {
+      this.wholeResponse = response.data.Search
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
